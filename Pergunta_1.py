0a2120a38fddfddc004929bb5673a297d30afa88
JardelBrandon <jardelbrandon@hotmail.com>
2022-08-05 17:23:26 -0300

    Resposta_1
    Adicionando resolução da primeira pergunta do processo seletivo:
    
    """
    Dado um vetor que guarda o valor de faturamento diário de uma distribuidora de todos os dias de um ano, faça um programa, na linguagem que desejar, que calcule e retorne:
    
    O menor valor de faturamento ocorrido em um dia do ano;
    
    O maior valor de faturamento ocorrido em um dia do ano;
    
    Número de dias no ano em que o valor de faturamento diário foi superior à média anual.
    
    IMPORTANTE:
    
    Considerar o vetor já carregado com as informações de valor de faturamento.
    
    Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média.
    
    Utilize o algoritmo mais veloz que puder definir.
    """

--- Pergunta_1.py
+++ Pergunta_1.py
@@ -1,10 +1,31 @@
-indice = 13
-soma = 0
-k = 0
+import json
 
-while k < indice:
-    k += 1
-    soma += k
-    print("iteracao: ", k, "\t|\tvalor da soma: ", soma)
+with open('assets/dados.json', 'r') as JSON:
+    json_vetor = json.load(JSON)
 
-print("\nSoma = ", soma)
+menor_valor_de_faturamento = float("inf")
+maior_valor_de_faturamento = 0
+valor_medio_de_faturamento = 0
+dias_uteis = 0
+quantidade_dias_superiores_da_media = 0
+
+for dados in json_vetor:
+    if dados['valor'] != 0:
+        dias_uteis += 1
+        valor_medio_de_faturamento += dados['valor']
+
+        if menor_valor_de_faturamento > dados['valor']:
+            menor_valor_de_faturamento = dados['valor']
+        if maior_valor_de_faturamento < dados['valor']:
+            maior_valor_de_faturamento = dados['valor']
+
+valor_medio_de_faturamento /= dias_uteis
+
+for dados in json_vetor:
+    if dados['valor'] > valor_medio_de_faturamento:
+        quantidade_dias_superiores_da_media += 1
+
+print("Menor valor de faturamento ocorrido em um dia do ano: ", menor_valor_de_faturamento)
+print("Maior valor de faturamento ocorrido em um dia do ano: ", maior_valor_de_faturamento)
+print("Quantidade de dias com o faturamento superior a média anual: ", quantidade_dias_superiores_da_media)
+
